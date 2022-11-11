9a81b720b3a21a03c4e52f21aab816a28824469f
yucellsarikaya <berkaykata@gmail.com>
2022-11-11 13:06:34 +0300

    Update Question(217)contains-duplicate.js

--- Question(217)contains-duplicate.js
+++ Question(217)contains-duplicate.js
@@ -1,6 +1,7 @@
 /**
  * @param {number[]} nums
  * @return {boolean}
+ https://leetcode.com/submissions/detail/841294012/
  */
 var containsDuplicate = function(nums) {
     const ayniOlanlariCikar = new Set(nums);
